5c168045820a3da0e51fd22734d15fa0682b6f00
BoviliusMeidi <boviliusbm@gmail.com>
2024-11-17 22:00:23 +0700

    add : authenticationToken

---
+++ backend/middlewares/authenticationToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authenticationToken = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'Access denied' });
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid token' });
+    }
+};
+
+module.exports = {authenticationToken};
