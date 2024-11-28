4186d3209fd0fc2516088a459ca366f8eb6aa426
BoviliusMeidi <boviliusbm@gmail.com>
2024-11-28 13:43:56 +0700

    changes : favorite controller for integration with server

--- frontend/controllers/favoriteController.js
+++ frontend/controllers/favoriteController.js
@@ -1,5 +1,4 @@
-app.controller('FavoriteController', function ($scope, $http, $location) {
-    console.log(localStorage);
+app.controller('FavoriteController', function ($scope, $http) {
     $scope.message = '';
 
     $http.get('http://127.0.0.1:3000/api/check-session', { withCredentials: true })
@@ -9,14 +8,13 @@ app.controller('FavoriteController', function ($scope, $http, $location) {
         .catch(function (error) {
             localStorage.setItem('showAlert', true);
             localStorage.setItem('visitedFavorite', true);
-            window.location.href = 'http://127.0.0.1:5500/frontend/views/login.html';
+            window.location.href = 'http://127.0.0.1:5500/frontend/views/authentication/login.html';
         });
 
     $scope.logout = function () {
-        $http.post('http://127.0.0.1:3000/api/logout')
-            .then(function (response) {
-                localStorage.removeItem('user');
-                window.location.href = 'http://127.0.0.1:5500/frontend/views/login.html';
+        $http.delete('http://127.0.0.1:3000/api/logout', {withCredentials: true })
+            .then(function () {
+                window.location.href = 'http://127.0.0.1:5500/frontend/views/authentication/login.html';
             })
             .catch(function (error) {
                 console.error("Error during logout:", error);
