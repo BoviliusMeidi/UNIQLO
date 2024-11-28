d930b5dd33a565f9ca4340d77949b80f33cf8f1f
BoviliusMeidi <boviliusbm@gmail.com>
2024-11-28 13:32:52 +0700

    add : app.js with priceFormat and dateFormat

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,1 +1,21 @@
 const app = angular.module('myApp', ['ngRoute']);
+const API_URL = "http://127.0.0.1:3000/api/";
+const UI_URL = "http://127.0.0.1:5500/frontend/views/";
+
+app.filter('priceFormat', function () {
+    return function (price) {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'decimal',
+            maximumFractionDigits: 3
+        }).format(price);
+    };
+});
+
+app.filter('dateFormat', function() {
+    return function(input) {
+        if (input) {
+            return input.split('T')[0]; // Extract just the date part
+        }
+        return input;
+    };
+});
