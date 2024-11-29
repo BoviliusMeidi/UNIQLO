253e7623aad6b9e6ce08631c05f16ec88d0bf0ee
BoviliusMeidi <boviliusbm@gmail.com>
2024-11-30 00:04:17 +0700

    add : create product, getproductbyid, updateproduct, deleteproduct, updateproductimage

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,16 +1,72 @@
 const db = require('../config/dbConfig');
 
+const createProduct = async (product_name, product_picture, category, price, stock, size, description) => {
+    try {
+        const [product] = await db('products')
+            .insert({
+                product_name,
+                product_picture,
+                category,
+                price,
+                stock,
+                size,
+                description
+            })
+            .returning('*');
+        return product;
+    } catch (error) {
+        console.error('Error creating product:', error);
+        throw error;
+    }
+};
+
 const getProducts = async () => {
     try {
         const products = await db('products').select('*');
-        return products;
+        return products.length > 0 ? products : null;
     } catch (error) {
         console.error('Error fetching products:', error);
         throw error;
     }
 };
 
-const updateProduct = async (productId, imageUrl) => {
+const getProductById = async (productId) => {
+    try {
+        const products = await db('products').where({ product_id: productId }).select('*');
+        return products.length > 0 ? products[0] : null;
+    } catch (error) {
+        console.error('Error fetching product by ID:', error);
+        throw error;
+    }
+};
+
+const updateProduct = async (productId, name, price, description, stock, size, category) => {
+    try {
+        const updatedProduct = await db('products')
+            .where({ product_id: productId })
+            .update({ product_name: name, category, price, stock, size, description})
+            .returning('*');
+        return updatedProduct.length > 0 ? updatedProduct[0] : null;
+    } catch (error) {
+        console.error('Error updating product:', error);
+        throw error;
+    }
+};
+
+const deleteProduct = async (productId) => {
+    try {
+        const deletedProduct = await db('products')
+            .where({ product_id: productId })
+            .del()
+            .returning('*');
+        return deletedProduct.length > 0 ? deletedProduct[0] : null;
+    } catch (error) {
+        console.error('Error deleting product:', error);
+        throw error;
+    }
+};
+
+const updateProductImage = async (productId, imageUrl) => {
     try {
         const updatedProduct = await db('products')
             .where({ product_id: productId })
@@ -18,9 +74,16 @@ const updateProduct = async (productId, imageUrl) => {
             .returning('*');
         return updatedProduct.length > 0 ? updatedUser[0] : null;
     } catch (error) {
-        console.error('Error upload poduct picture:', error);
+        console.error('Error upload product picture:', error);
         throw error;
     }
 };
 
-module.exports = {getProducts, updateProduct };
+module.exports = {
+    createProduct,
+    getProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+    updateProductImage
+};
