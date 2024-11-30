133f3b2b0c6b7aac46fad08484845016cd00df31
BoviliusMeidi <boviliusbm@gmail.com>
2024-12-01 00:07:25 +0700

    add : filter, pagination, and search

--- frontend/controllers/adminController.js
+++ frontend/controllers/adminController.js
@@ -1,37 +1,93 @@
 const app = angular.module('adminApp', []);
 const API_ADMIN_URL = "http://127.0.0.1:3000/api/admin/";
 
+app.filter('priceFormat', function () {
+  return function (price) {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'decimal',
+      maximumFractionDigits: 3
+    }).format(price);
+  };
+});
+
+app.filter('ifNull', function () {
+  return function (input) {
+    if (input == null || input === '') {
+      return 'No Data';
+    }
+    return input;
+  };
+});
+
+app.filter('dateFormat', function () {
+  return function (input) {
+    if (input) {
+      return input.split('T')[0];
+    }
+    return input;
+  };
+});
+
 app.controller('AdminController', function ($scope, $http) {
+  $scope.currentPage = 1;
+  $scope.pageSize = 10;
   $scope.products = [];
   $scope.users = [];
   $scope.carts = [];
+  $scope.searchQuery = '';
   $scope.newProduct = {};
+  $scope.categories = ['MAN', 'WOMAN', 'KIDS'];
+  $scope.sizes = ['S', 'M', 'L', 'XL', 'XXL'];
 
-  $scope.onFileChange = function () {
-    const fileInput = document.getElementById('product_picture');
-    const file = fileInput.files[0];
-
-    if (file) {
-      const formData = new FormData();
-      formData.append('image', file);
-
-      $http.post(API_ADMIN_URL + 'product/upload', formData, {
-        withCredentials: true,
-        headers: {
-          'Content-Type': undefined,
-        },
+  // function for load data
+  function loadData() {
+    $http.get(API_ADMIN_URL + 'products', { withCredentials: true })
+      .then(function (response) {
+        $scope.products = response.data.listProducts.map(product => ({
+          ...product,
+          isEditing: false,
+        }));
+        $scope.totalProducts = $scope.products.length;
       })
-        .then(function (response) {
-          $scope.newProduct.product_picture = '../../backend/uploads/products/' + response.data.filename;
-          $scope.message = 'Product image uploaded successfully.';
-        })
-        .catch(function (error) {
-          console.error('Error uploading image:', error);
-          $scope.message = 'Error uploading image. Please try again.';
-        });
-    }
-  };
+      .catch(function (error) {
+        console.error('Error fetching products:', error);
+        alert('Failed to load products.');
+      });
 
+    $http.get(API_ADMIN_URL + 'users', { withCredentials: true })
+      .then(function (response) {
+        $scope.users = response.data.listUsers.map(user => ({
+          ...user,
+          isEditing: false,
+        }));
+        $scope.totalUsers = $scope.users.length
+      })
+      .catch(function (error) {
+        console.error('Error fetching users:', error);
+        alert('Failed to load users.');
+      });
+
+    $http.get(API_ADMIN_URL + 'carts', { withCredentials: true })
+      .then(function (response) {
+        if (response.data.listCarts == null) {
+          $scope.message = 'Error creating product. Please try again.';
+        } else {
+          $scope.carts = response.data.listCarts.map(cart => ({
+            ...cart,
+            isEditing: false,
+          }));
+          $scope.totalCarts = $scope.carts.length;
+        }
+      })
+      .catch(function (error) {
+        console.error('Error fetching carts:', error);
+        alert('Failed to load carts.');
+      });
+  }
+
+  loadData();
+
+  // create product
   $scope.createProduct = function (event) {
     event.preventDefault();
     if (!$scope.newProduct.product_picture) {
@@ -50,50 +106,12 @@ app.controller('AdminController', function ($scope, $http) {
       });
   };
 
-  $http.get(API_ADMIN_URL + 'products', { withCredentials: true })
-    .then(function (response) {
-      $scope.products = response.data.listProducts.map(product => ({
-        ...product,
-        isEditing: false,
-      }));
-    })
-    .catch(function (error) {
-      console.error('Error fetching products:', error);
-      alert('Failed to load products.');
-    });
-
-  $http.get(API_ADMIN_URL + 'users', { withCredentials: true })
-    .then(function (response) {
-      $scope.users = response.data.listUsers.map(user => ({
-        ...user,
-        isEditing: false,
-      }));
-    })
-    .catch(function (error) {
-      console.error('Error fetching users:', error);
-      alert('Failed to load users.');
-    });
-
-  $http.get(API_ADMIN_URL + 'carts', { withCredentials: true })
-    .then(function (response) {
-      if (response.data.listCarts == null) {
-        $scope.message = 'Error creating product. Please try again.';
-      } else {
-        $scope.carts = response.data.listCarts.map(cart => ({
-          ...cart,
-          isEditing: false,
-        }));
-      }
-    })
-    .catch(function (error) {
-      console.error('Error fetching carts:', error);
-      alert('Failed to load carts.');
-    });
-
+  // edit product
   $scope.editProduct = function (product) {
     product.isEditing = true;
   };
 
+  // save product
   $scope.saveProduct = function (product) {
     $http.put(API_ADMIN_URL + `products/${product.product_id}`, product, { withCredentials: true })
       .then(function () {
@@ -106,6 +124,7 @@ app.controller('AdminController', function ($scope, $http) {
       });
   };
 
+  // delete function
   $scope.deleteProduct = function (productId) {
     if (confirm('Are you sure you want to delete this product?')) {
       $http.delete(API_ADMIN_URL + `products/${productId}`, { withCredentials: true })
@@ -144,6 +163,88 @@ app.controller('AdminController', function ($scope, $http) {
         .catch(function (error) {
           console.error('Error deleting user:', error);
           alert('Failed to delete user.');
+        });
+    }
+  };
+
+  // pagination function
+  $scope.getPagedData = function (data) {
+    const startIndex = ($scope.currentPage - 1) * $scope.pageSize;
+    return data.slice(startIndex, startIndex + $scope.pageSize);
+  };
+
+  $scope.changePage = function (page, type) {
+    if (page > 0 && page <= $scope.getTotalPages(type)) {
+      $scope.currentPage = page;
+    }
+  };
+
+  $scope.getTotalPages = function (type) {
+    if (type === 'products') {
+      return Math.ceil($scope.products.length / $scope.pageSize);
+    } else if (type === 'users') {
+      return Math.ceil($scope.users.length / $scope.pageSize);
+    } else if (type === 'carts') {
+      return Math.ceil($scope.carts.length / $scope.pageSize);
+    }
+    return 0;
+  };
+
+  // filtered function
+  $scope.filteredProducts = function () {
+    const query = $scope.searchQuery.toLowerCase();
+    return $scope.getPagedData($scope.products.filter(product => {
+      return (
+        product.product_name.toLowerCase().includes(query) ||
+        product.description.toLowerCase().includes(query)
+      );
+    }));
+  };
+
+  $scope.filteredUsers = function () {
+    const query = $scope.searchQuery.toLowerCase();
+    return $scope.getPagedData($scope.users.filter(user => {
+      return (
+        user.username.toLowerCase().includes(query) ||
+        user.email.toLowerCase().includes(query) ||
+        user.country.toLowerCase().includes(query) ||
+        user.address.toLowerCase().includes(query)
+      );
+    }));
+  };
+
+  $scope.filteredCarts = function () {
+    const query = $scope.searchQuery.toLowerCase();
+    return $scope.getPagedData($scope.carts.filter(cart => {
+      return (
+        cart.username.toLowerCase().includes(query) ||
+        cart.email.toLowerCase().includes(query) ||
+        cart.product_name.toLowerCase().includes(query)
+      );
+    }));
+  };
+
+  $scope.onFileChange = function () {
+    const fileInput = document.getElementById('product_picture');
+    const file = fileInput.files[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append('image', file);
+
+      $http.post(API_ADMIN_URL + 'product/upload', formData, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': undefined,
+        },
+      })
+        .then(function (response) {
+          $scope.newProduct.product_picture = '../../backend/uploads/products/' + response.data.filename;
+          $scope.message = 'Product image uploaded successfully.';
+        })
+        .catch(function (error) {
+          console.error('Error uploading image:', error);
+          $scope.message = 'Error uploading image. Please try again.';
         });
     }
   };
