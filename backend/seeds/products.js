c95b4009b9357b001b25842079a7332a86a947e4
aldianYHS <aldian.535230139@stu.untar.ac.id>
2024-12-02 19:33:43 +0700

    change: category misspelled

--- backend/seeds/products.js
+++ backend/seeds/products.js
@@ -5,7 +5,10 @@
 exports.seed = async function (knex) {
   await knex('products').del()
   await knex('products').insert([
-    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: 'Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'MAN', price: '399000', stock: '100', size: 'XL', description: 'Kemeja Panjang' },
-    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: 'Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'MAN', price: '399000', stock: '100', size: 'S', description: 'Kemeja Panjang' },
+    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: 'Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'MEN', price: '399000', stock: '100', size: 'XL', description: 'Kemeja Panjang' },
+    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: 'Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'MEN', price: '399000', stock: '100', size: 'S', description: 'Kemeja Panjang' },
+    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: '[KIDS] Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'KIDS', price: '399000', stock: '100', size: 'S', description: 'Kemeja Panjang' },
+    { product_picture: '../../assets/stores/products/KemejaFlannelILenganPanjangKotak(Button-Down).jpg', product_name: '[WOMAN]Kemeja Flannel I Lengan Panjang Kotak (Button-down)', category: 'WOMAN', price: '399000', stock: '100', size: 'S', description: 'Kemeja Panjang' },
+
   ]);
 };
